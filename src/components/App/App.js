1d8c13a8f2a06bc82bd4ee65f93d73e004641f55
mitch <sketch0306@gmail.com>
2020-06-17 22:43:33 -0400

    added app bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
-        
+
+      <AppBar position="fixed">
+        <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+    </IconButton>
+    <Typography variant="h6">
+      robo_thoughts
+    </Typography>
+ 
+        </Toolbar>
+      </AppBar>
+      <Toolbar />  
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
